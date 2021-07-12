fd5dc47f83a73eff1db19cb07ef317d1aa7cc1ea
Alex Anderson <alexanderson1993@gmail.com>
2021-07-12 12:10:15 -0400

    Finish up the checkout URL

--- functions/checkout.ts
+++ functions/checkout.ts
@@ -6,31 +6,33 @@ const stripe = stripeInit(process.env.STRIPE_SECRET_KEY || "");
 
 const handler: Handler = async (event, context) => {
   if (event.httpMethod === "POST") {
-    console.log(event.body);
-    // const price = JSON.parse(event.body || "{}").price || 500;
-    // const session = await stripe.checkout.sessions.create({
-    //   payment_method_types: ["card"],
-    //   line_items: [
-    //     {
-    //       price_data: {
-    //         currency: "usd",
-    //         product_data: {
-    //           name: "Reaction Purchase",
-    //         },
-    //         unit_amount: price,
-    //       },
-    //       quantity: 1,
-    //     },
-    //   ],
-    //   mode: "payment",
-    //   success_url: `https://reaction.fyreworks.us?success=true`,
-    //   cancel_url: `https://reaction.fyreworks.us?canceled=true`,
-    // });
-    // return {
-    //   statusCode: 303,
+    const body = new URLSearchParams(event.body || "");
 
-    //   headers: { location: session.url },
-    // };
+    const price = parseInt(body.get("price") || "5", 10) * 100;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Reaction Purchase",
+            },
+            unit_amount: price,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `https://reaction.fyreworks.us?success=true`,
+      cancel_url: `https://reaction.fyreworks.us?canceled=true`,
+    });
+    return {
+      statusCode: 303,
+
+      headers: { location: session.url },
+    };
   }
   return {
     statusCode: 404,
